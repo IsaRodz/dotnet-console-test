b14b413994b092a9d89abeb1eabf1665e0c5b53b
IsaRodz <rodrigueziriarte96@gmail.com>
2021-11-05 19:43:15 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace dotnet_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string API_URL = "https://jsonplaceholder.typicode.com/posts?_limit=10";
+            var client = new WebClient();
+            var data = client.DownloadString(API_URL);
+            dynamic posts = JsonConvert.DeserializeObject(data);
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine(post.id + ": " + post.title);
+            }
+        }
+    }
+}
